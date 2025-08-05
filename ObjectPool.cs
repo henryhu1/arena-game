a71d1da237aa23f9e5f17f33be48b45b1572d811
Henry Hu <huhenry50@gmail.com>
2025-08-04 23:33:26 -0400

    add object pooling

---
+++ ObjectPool.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool
+{
+    private GameObject prefab;
+    private Transform parent;
+    private Queue<GameObject> pool = new();
+
+    public ObjectPool(GameObject prefab, int initialSize, Transform parent = null)
+    {
+        this.prefab = prefab;
+        this.parent = parent;
+
+        for (int i = 0; i < initialSize; i++)
+        {
+            GameObject obj = Object.Instantiate(prefab, parent);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject Get(Vector3 position, Quaternion rotation)
+    {
+        GameObject obj = pool.Count > 0 ? pool.Dequeue() : Object.Instantiate(prefab, parent);
+        obj.transform.SetPositionAndRotation(position, rotation);
+        obj.SetActive(true);
+        return obj;
+    }
+
+    public void Return(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
---
+++ ObjectPoolManager.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    public static ObjectPoolManager Instance { get; private set; }
+
+    private Dictionary<GameObject, ObjectPool> poolMap = new();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+    }
+
+    public void CreatePool(GameObject prefab, int size)
+    {
+        if (!poolMap.ContainsKey(prefab))
+        {
+            poolMap[prefab] = new ObjectPool(prefab, size, transform);
+        }
+    }
+
+    public GameObject Spawn(GameObject prefab, Vector3 position, Quaternion rotation)
+    {
+        if (!poolMap.ContainsKey(prefab))
+            CreatePool(prefab, 1);
+
+        return poolMap[prefab].Get(position, rotation);
+    }
+
+    public void Despawn(GameObject obj, GameObject prefab)
+    {
+        if (poolMap.ContainsKey(prefab))
+        {
+            poolMap[prefab].Return(obj);
+        }
+        else
+        {
+            Destroy(obj); // fallback
+        }
+    }
+}
