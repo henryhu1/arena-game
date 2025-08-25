07ca86917e7deac54f67c0aa009717299dbfe3fc
Henry Hu <huhenry50@gmail.com>
2025-08-24 23:52:16 -0400

    random skeleton stats upon instantiation during runtime

--- Enemies/Skeleton.cs
+++ Enemies/Skeleton.cs
@@ -1,3 +1,11 @@
+using UnityEngine;
+
 public class Skeleton : EnemyControllerBase
 {
+    protected override void Awake()
+    {
+        enemyStats.sizeMultiplier = Random.Range(enemyStats.SizeMultiplierMin(), enemyStats.SizeMultiplierMax());
+
+        base.Awake();
+    }
 }
