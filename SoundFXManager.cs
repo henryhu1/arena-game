165d6941d4d12a9ade416c539bb2f1f0c113dc9e
Henry Hu <huhenry50@gmail.com>
2025-09-05 20:59:23 -0400

    use sound fx manager to play audio effects (bb6b382)

--- SoundFXManager.cs
+++ SoundFXManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class AudioEffect
+{
+    public AudioEffectSO audioData;
+    public Vector3EventChannelSO triggerEvent;
+}
 
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager Instance;
 
     [SerializeField] private AudioSource soundFXObject;
+    [SerializeField] private List<AudioEffect> effects;
 
     const int k_initialPoolSize = 10;
 
@@ -21,6 +30,31 @@ public class SoundFXManager : MonoBehaviour
     private void Start()
     {
         ObjectPoolManager.Instance.CreatePool(soundFXObject.gameObject, k_initialPoolSize);
+    }
+
+    private void OnEnable()
+    {
+        foreach (AudioEffect effect in effects)
+        {
+            if (effect.triggerEvent != null)
+                effect.triggerEvent.OnPositionEventRaised += pos => HandlePlay(effect, pos);
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (AudioEffect effect in effects)
+        {
+            if (effect.triggerEvent != null)
+                effect.triggerEvent.OnPositionEventRaised -= pos => HandlePlay(effect, pos);
+        }
+    }
+
+    private void HandlePlay(AudioEffect effect, Vector3 pos)
+    {
+        if (effect.audioData == null) return;
+
+        PlaySoundFXClip(effect.audioData.GetRandomClip(), pos, 0.6f);
     }
 
     public void PlaySoundFXClip(AudioClip audioClip, Vector3 spawnPos, float volume)
