777f85f94af3fe3e0ff5bcdc2ea8acc1d742ffce
Henry Hu <huhenry50@gmail.com>
2025-09-13 19:22:20 -0400

    UI: pulsate countdown color on low time

--- UI/Countdown.cs
+++ UI/Countdown.cs
@@ -1,10 +1,27 @@
+using DG.Tweening;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private FloatVariable countdownTimer;
+    [SerializeField] private Image background;
+
+    [SerializeField] private float warningTimeAt = 10f;
+
+    [SerializeField] private float colorPulseTime = 1.5f;
+
+    private Color originalColor;
+
+    private Coroutine timeWarning;
+
+    private void Awake()
+    {
+        originalColor = background.color;
+    }
 
     private void Start()
     {
@@ -26,6 +43,32 @@ public class Countdown : MonoBehaviour
         float timer = Mathf.Max(time, 0);
         float minutes = Mathf.Floor(timer / 60);
         float seconds = Mathf.Floor(timer % 60);
-        timerText.text = $"{(minutes > 0 ? $"{minutes}:" : "")}{(seconds < 10 ? "0" : "")}{seconds}";
+        timerText.text = $"{(minutes > 0 ? $"{minutes}:" : "")}{(0 < seconds && seconds < 10 ? "0" : "")}{seconds}";
+
+        if (0 < time && time <= warningTimeAt)
+        {
+            timeWarning ??= StartCoroutine(LowTimeWarning());
+        }
+        else
+        {
+            background.color = originalColor;
+            if (timeWarning != null)
+            {
+                StopCoroutine(timeWarning);
+                timeWarning = null;
+            }
+        }
+    }
+
+    private IEnumerator LowTimeWarning()
+    {
+        float numberOfPulses = 0;
+        while (true)
+        {
+            Color toColor = numberOfPulses % 2 == 0 ? Color.red : originalColor;
+            background.DOColor(toColor, colorPulseTime);
+            yield return new WaitForSeconds(colorPulseTime);
+            numberOfPulses++;
+        }
     }
 }
